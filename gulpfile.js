e0ce4a2bc856811d3a31c90952c212e11ee80423
Erick Soto <ericks@homebrewagency.com>
2014-03-28 16:22:54 -0700

    add uglification

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var util = require('gulp-util');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
+var minifyHTML = require('gulp-minify-html');
 
 var paths = {
     src: './src',
@@ -9,7 +11,8 @@ var paths = {
 };
 
 var files = {
-    scripts: ['./src/scripts/**/*.js']
+    scripts: ['./src/scripts/**/*.js'],
+    html: ['./src/**/*.html']
 };
 
 gulp.task('scripts', function (cb) {
@@ -19,13 +22,23 @@ gulp.task('scripts', function (cb) {
             insertGlobals: false
         }))
         .pipe(concat('build.js'))
+        .pipe(uglify({outSourceMap:true}))
         .pipe(gulp.dest(paths.dist+'/js'));
+});
+
+gulp.task('html', function() {
+    var opts = {comments:true, spare:true};
+    gulp.src(paths.src + '/index.html')
+        .pipe(minifyHTML(opts))
+        .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('watch', function () {
     
     gulp.watch(files.scripts, ['scripts']);
 
+    gulp.watch(files.html, ['html']);
+
 });
 
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts', 'html', 'watch']);
